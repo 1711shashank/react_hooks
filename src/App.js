43951fd47556579c5959554871e23a342e392edd
Kumar Shashank <1711shashank@gmail.com>
2022-07-25 22:15:04 +0530

    toggle between values using useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [name, setName] = useState('Kumar');
+
+  const changeName = () => {
+    name === 'Kumar' ? setName('Shashank') : setName('Kumar');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2>{name}</h2>
+      <button onClick={changeName}> click me </button>
+    </>
+  )
 }
 
-export default App;
+export default App
