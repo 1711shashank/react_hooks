d5550516e9f377016520784de6c89556496622aa
Kumar Shashank <1711shashank@gmail.com>
2022-08-26 19:19:21 +0530

    loading....

---
+++ src/component/ProfileCards.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+function ProfileCards({usersData}) {
+    return (
+        <div  className='profileCards'>
+            {
+                usersData.map((curUserData) => {
+                    return (
+                        <div key={curUserData._id} className='profileCard'>
+
+                            <div><img src={curUserData.avatar_url} /></div>
+
+                            <div><h3> {curUserData.name} </h3></div>
+
+                            <div className='profileCard__links'>
+                                <a href={curUserData.linkedin_ID} target="_blank">
+                                    <LinkedInIcon size="2x" />
+                                </a>
+                                <a href={curUserData.github_ID} target="_blank">
+                                    <GitHubIcon size="2x" />
+                                </a>
+                                <a href={curUserData.insta_ID} target="_blank">
+                                    <InstagramIcon size="2x" />
+                                </a>
+                            </div>
+
+                        </div>
+                    );
+                })
+            }
+        </div>
+    )
+}
+
+export default ProfileCards;
