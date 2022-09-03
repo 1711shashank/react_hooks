0cceb4eb3421623cae12c3368fe76b09b91c136c
Kumar Shashank <1711shashank@gmail.com>
2022-09-03 12:10:35 +0530

    UnconrolledFrom

---
+++ src/component/UnconrolledFrom.js
@@ -0,0 +1,30 @@
+import React,{ useRef, useState } from 'react'
+
+const UnconrolledFrom = () => {
+
+    //it like a useState only and its preserve the value.  NO re-render
+    const myName = useRef(null);
+    const [show, setShow] = useState(false);
+
+    const SubmitForm = (e) => {
+        e.preventDefault();
+        
+        const name = myName.current.value;
+        (name === "") ? alert("plz fill the data") : setShow(true);
+    }
+
+    return (
+        <div>
+            <form action='' onSubmit={SubmitForm}>
+                <div>
+                    <label> Enter Your Name </label>
+                    <input type='text' id='myName' ref={ myName } />
+                </div>
+                <button> Submit </button>
+            </form>
+            <p> { (show) ? `Your Name is ${myName.current.value}` : '' }  </p>
+        </div>
+    )
+}
+
+export default UnconrolledFrom
